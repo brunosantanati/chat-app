805c961d75e0f5d3ed0fd87699a8c67437386bd9
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2024-01-30 13:33:53 -0300

    Deal with Socket.io events

--- src/index.js
+++ src/index.js
@@ -12,8 +12,21 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0
+
+// server (emit) -> client (receive) - countUpdated
+// client (emit) -> server (receive) - increment
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        //socket.emit('countUpdated', count) //emit event to only one client/connection
+        io.emit('countUpdated', count) //emit event to all connections
+    })
 })
 
 server.listen(port, () => {
